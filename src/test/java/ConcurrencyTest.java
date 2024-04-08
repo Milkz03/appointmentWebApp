7c1287d4694705d66a8bb41163c1661754c7adab
Frances <97236341+frncs-dc@users.noreply.github.com>
2024-04-08 22:54:58 +0800

    test: made test for concurrent updates and reads on same data (Case 2)

--- src/test/java/ConcurrencyTest.java
+++ src/test/java/ConcurrencyTest.java
@@ -9,6 +9,77 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class ConcurrencyTest {
     final updateAppointment updateAppointment = new updateAppointment();
+    final readAppointment readAppointment = new readAppointment();
+    @Test // Case #2
+    public void two_concurrent_updates_and_read_same_row() throws InterruptedException, SQLException {
+        updateAppointment.appointmentID = "test1";
+        readAppointment.appointmentID = "test1";
+
+        // Given two concurrent transactions
+        //one updates
+        Thread thread1 = new Thread(() -> {
+            updateAppointment.appointment.connectionNumber = 1;
+            updateAppointment.patientID = "Patient1";
+            updateAppointment.doctorID = "Doctor1";
+            updateAppointment.apptStatus = "Status1";
+            updateAppointment.timeQueued = "2024-04-08T10:00:00";
+            updateAppointment.startTime = "2024-04-08T10:30:00";
+            updateAppointment.endTime = "2024-04-08T11:00:00";
+            updateAppointment.consultationType = "ConsultationType1";
+            updateAppointment.virtualConsultation = 1;
+            updateAppointment.updateAppointments();
+        });
+        // the other reads
+        Thread thread2 = new Thread(() -> {
+            readAppointment.appointment.connectionNumber = 0;
+            readAppointment.patientID = "Patient2";
+            readAppointment.doctorID = "Doctor2";
+            readAppointment.apptStatus = "Status2";
+            readAppointment.timeQueued = "2024-04-08T11:00:00";
+            readAppointment.startTime = "2024-04-08T11:30:00";
+            readAppointment.endTime = "2024-04-08T12:00:00";
+            readAppointment.consultationType = "ConsultationType2";
+            readAppointment.virtualConsultation = 0;
+            readAppointment.infoAppointments();
+        });
+
+        thread1.start();
+        thread2.start();
+
+        thread1.join();
+        thread2.join();
+
+        // thread1 updates the appointment -> once done, thread2 updates the appointment
+        Connection conn = null;
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            conn = DriverManager.getConnection("jdbc:mysql://ccscloud.dlsu.edu.ph:20183/apptMCO2?user=advdb");
+
+            PreparedStatement pstmt = conn.prepareStatement("SELECT * FROM appointments WHERE appointmentID=?");
+            pstmt.setString(1, "test1");
+            ResultSet resultSet = pstmt.executeQuery();
+
+            // Then the appointment information would have the information from the second thread
+            if (resultSet.next()) {
+                assertEquals("Patient1", resultSet.getString("patientID"));
+                assertEquals("Doctor1", resultSet.getString("doctorID"));
+                assertEquals("Status1", resultSet.getString("apptStatus"));
+
+            } else {
+                fail("Appointment not found in the database");
+            }
+
+            pstmt.close();
+            conn.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            fail("An exception occurred while querying the database");
+            if (conn != null) {
+                conn.close();
+            }
+        }
+    }
     @Test // Case #3
     public void two_concurrent_updates_same_row() throws InterruptedException, SQLException {
         updateAppointment.appointmentID = "test1";
